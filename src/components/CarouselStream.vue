436a5df9e728f065d3ac12d024e10b9c6023a2cd
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-09-25 00:05:30 -0500

    Start with Carousel component

---
+++ src/components/CarouselStream.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  items: Array
+})
+</script>
+
+<template>
+  <h1 class="carousel-title">Explora nuestras películas y series recomendadas</h1>
+
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <!-- Imágenes dinámicas con v-for -->
+    <div class="carousel-inner">
+      <div
+        v-for="(item, index) in props.items"
+        :key="index"
+        :class="['carousel-item', { active: index === 0 }]"
+      >
+        <img :src="item.scr" class="d-block w-100" :alt="item.nombre" loading="lazy" />
+        <div class="carousel-caption d-none d-md-block">
+          <h5>{{ item.nombre }}</h5>
+          <p>{{ item.tipo }} - IMDb: {{ item.imdb }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Controles del carousel -->
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleFade"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleFade"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.carousel-title {
+  margin-bottom: 56px;
+  text-align: center;
+  font-size: 2rem;
+}
+</style>
--- src/components/HomeApp.vue
+++ src/components/HomeApp.vue
@@ -1,6 +1,28 @@
 <script setup>
-import LayoutApp from '@/components/LayoutApp.vue'
+import { onMounted, ref } from 'vue'
+import LayoutApp from '@/layouts/LayoutApp.vue'
 import NavBar from './NavBar.vue'
+import CarouselStream from './CarouselStream.vue'
+
+const API_URL = import.meta.env.VITE_APP_API_URL
+const items = ref(null)
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.json()
+    items.value = data.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
 </script>
 
 <template>
@@ -8,7 +30,9 @@ import NavBar from './NavBar.vue'
     <template #navbar>
       <NavBar />
     </template>
-    <template #carousel-img>Aquí va mi carousel</template>
+    <template #carousel-img>
+      <CarouselStream :items="items" />
+    </template>
     <template #form-random-movie>Mi ruleta y mi formulario</template>
     <template #card-movie-selected>Mi PELICULA/SERIE</template>
     <template #footer>By Juan</template>
