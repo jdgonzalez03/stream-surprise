bb423dbc2aeda6fe2b6944bbb4f98bc970b271d4
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-09-25 00:04:26 -0500

    Create folder with layouts

---
+++ src/layouts/LayoutApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class="navbar-slot">
+      <slot name="navbar"></slot>
+    </section>
+    <section class="carousel-slot">
+      <slot name="carousel-img"></slot>
+    </section>
+    <section class="form-random-movie-slot">
+      <slot name="form-random-movie"></slot>
+    </section>
+    <section class="card-movie-selected-slot">
+      <slot name="card-movie-selected"></slot>
+    </section>
+    <section class="footer-slot">
+      <slot name="footer"></slot>
+    </section>
+  </div>
+</template>
