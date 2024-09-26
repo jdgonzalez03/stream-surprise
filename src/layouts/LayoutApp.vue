926efc5fee3b608f4cfb627e7a753e3e0a559fbe
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-09-25 22:18:36 -0500

    Changes ids

--- src/layouts/LayoutApp.vue
+++ src/layouts/LayoutApp.vue
@@ -6,8 +6,8 @@
     <section class="carousel-slot">
       <slot name="carousel-img"></slot>
     </section>
-    <section class="form-random-movie-slot">
-      <slot name="form-random-movie"></slot>
+    <section id="select-movie" class="select-movie-slot">
+      <slot name="select-movie"></slot>
     </section>
     <section class="card-movie-selected-slot">
       <slot name="card-movie-selected"></slot>
@@ -27,8 +27,14 @@
 @media (min-width: 1200px) {
   .carousel-slot {
     margin: 0 auto;
-
     max-width: 1200px;
   }
+}
+
+.select-movie-slot {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
